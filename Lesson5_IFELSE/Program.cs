eddad3c34636dc0b1a835430dda545c82dd987d5
rvalikov <r.valikov@mail.ru>
2022-11-19 23:08:08 +0300

    IFELSE

---
+++ Lesson5_IFELSE/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("input your name");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha"){
+    Console.WriteLine("WOW Masha");
+}
+else
+{
+    Console.WriteLine("Hello" );
+}
