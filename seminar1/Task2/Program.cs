587b5e3ed4076b2b14825bedfb31168dd4f5416a
rvalikov <r.valikov@mail.ru>
2022-11-22 21:19:56 +0300

    Task2

---
+++ seminar1/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task2 Программа принимает на вход два числа, и выдает, какое большее, а какое меньшее
+Console.Clear();
+Console.Write("Ввведите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввведите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+if(a>b) Console.WriteLine("Первое число больше");
+else Console.WriteLine("Второе число больше");
+
